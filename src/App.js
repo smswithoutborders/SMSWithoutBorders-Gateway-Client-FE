d77cfb9b5e246ea18f63b72a48336b634d7d5c17
nSedrickm <nsedrick101@gmail.com>
2021-01-20 23:23:19 +0100

    feat(header Ui shell): add dashboard shell headers and nav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
 import './App.scss';
 
-function App() {
+import {
+  HeaderContainer,
+  Header,
+  HeaderName,
+  SkipToContent,
+  HeaderGlobalAction,
+  HeaderGlobalBar,
+  HeaderMenuButton,
+  SideNav,
+  SideNavItems,
+  SideNavLink
+
+} from 'carbon-components-react';
+
+import {
+  AppSwitcher20,
+  Search20,
+  Notification20,
+  EmailNew32,
+  Grid32,
+  IotPlatform16
+} from '@carbon/icons-react';
+
+import { Route, Switch } from 'react-router-dom';
+import { Content } from 'carbon-components-react';
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HeaderContainer
+        render={({ isSideNavExpanded, onClickSideNavExpand }) => (
+          <>
+            <Header aria-label="Afkanerd C - Deck">
+              <SkipToContent />
+              <HeaderMenuButton
+                aria-label="Open menu"
+                isCollapsible
+                onClick={onClickSideNavExpand}
+                isActive={isSideNavExpanded}
+              />
+              <HeaderName href="#" prefix="">
+                C | Deck
+          </HeaderName>
+              <HeaderGlobalBar>
+                <HeaderGlobalAction
+                  aria-label="Search">
+                  <Search20 />
+                </HeaderGlobalAction>
+                <HeaderGlobalAction
+                  aria-label="Notifications">
+                  <Notification20 />
+                </HeaderGlobalAction>
+                <HeaderGlobalAction
+                  aria-label="App Switcher">
+                  <AppSwitcher20 />
+                </HeaderGlobalAction>
+              </HeaderGlobalBar>
+              <SideNav
+                aria-label="Side navigation"
+                isRail
+                defaultExpanded={true}
+                expanded={isSideNavExpanded}
+              >
+                <SideNavItems>
+                  <SideNavLink renderIcon={Grid32} href="#" large>
+                    Dashboard
+                </SideNavLink>
+                  <SideNavLink renderIcon={EmailNew32} href="#" large>
+                    SMS
+                </SideNavLink>
+                  <SideNavLink renderIcon={IotPlatform16} href="#" large>
+                    Modems
+                </SideNavLink>
+                </SideNavItems>
+              </SideNav>
+            </Header>
+          </>
+        )}
+      />
+      
+      <Content>
+        <Switch>
+          <Route exact path="/" />
+          <Route path="/sms" />
+          <Route path="/modems" />
+        </Switch>
+      </Content>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 
