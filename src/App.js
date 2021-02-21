d90e8cac77253603523f3f8fb4b1ec1afed1f08a
nSedrickm <nsedrick101@gmail.com>
2021-02-21 21:48:45 +0100

    feat(login): add login screen with toggle view

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+import React, { useState } from 'react';
 import './App.scss';
-
 import {
   Content,
   Header,
@@ -15,7 +15,6 @@ import {
   SideNavItems,
   SideNavLink,
 } from 'carbon-components-react';
-
 import {
   AppSwitcher20,
   Search20,
@@ -24,79 +23,93 @@ import {
   Grid32,
   IotPlatform16
 } from '@carbon/icons-react';
-
 import { Route, Switch, Link } from 'react-router-dom';
-
+import Login from './content/Login';
 import DashBoard from './content/Dashboard';
 import SMS from './content/SMS';
 import Modem from './content/Modem';
 import NewSMS from './components/NewSMS';
 import BulkSMS from './components/BulkSMS';
 
+const DashView = () => {
+  return (
+    <HeaderContainer
+      render={({ isSideNavExpanded, onClickSideNavExpand }) => (
+        <>
+          <Header aria-label="SMS without borders">
+            <SkipToContent />
+            <HeaderMenuButton
+              aria-label="Open menu"
+              onClick={onClickSideNavExpand}
+              isActive={isSideNavExpanded}
+            />
+            <HeaderName href="#" prefix="">
+              C | Deck
+          </HeaderName>
+            <HeaderNavigation aria-label="C | Deck">
+              <HeaderMenuItem href="#">API</HeaderMenuItem>
+              <HeaderMenuItem href="#">Docs</HeaderMenuItem>
+              <HeaderMenuItem href="#">About</HeaderMenuItem>
+            </HeaderNavigation>
+            <HeaderGlobalBar>
+              <HeaderGlobalAction
+                aria-label="Search">
+                <Search20 />
+              </HeaderGlobalAction>
+              <HeaderGlobalAction
+                aria-label="Notifications">
+                <Notification20 />
+              </HeaderGlobalAction>
+              <HeaderGlobalAction
+                aria-label="App Switcher">
+                <AppSwitcher20 />
+              </HeaderGlobalAction>
+            </HeaderGlobalBar>
+            <SideNav aria-label="Side navigation" expanded={isSideNavExpanded}>
+              <SideNavItems>
+                <SideNavLink large renderIcon={Grid32} element={Link} to="/dash">
+                  Dashboard
+          </SideNavLink>
+                <SideNavLink large renderIcon={EmailNew32} element={Link} to="/sms">
+                  SMS
+          </SideNavLink>
+                <SideNavLink large renderIcon={IotPlatform16} element={Link} to="/modem">
+                  Modems
+          </SideNavLink>
+              </SideNavItems>
+            </SideNav>
+          </Header>
+
+          <Content id="main-content" className="bx--col-lg-13 bx--offset-lg-3">
+            <Switch>
+              <Route exact path="/dash" component={DashBoard} />
+              <Route exact path="/sms" component={SMS} />
+              <Route exact path="/sms/:modem" />
+              <Route exact path="/new-sms" component={NewSMS} />
+              <Route exact path="/bulk-sms" component={BulkSMS} />
+              <Route exact path="/modem" component={Modem} />
+            </Switch>
+          </Content>
+        </>
+      )}
+    />
+  );
+};
+
 const App = () => {
+
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+  let view;
+
+  if (isLoggedIn) {
+    view = <DashView />
+  } else {
+    view = <Login />
+  }
 
   return (
     <>
-      <HeaderContainer
-        render={({ isSideNavExpanded, onClickSideNavExpand }) => (
-          <>
-            <Header aria-label="SMS without borders">
-              <SkipToContent />
-              <HeaderMenuButton
-                aria-label="Open menu"
-                onClick={onClickSideNavExpand}
-                isActive={isSideNavExpanded}
-              />
-              <HeaderName href="#" prefix="">
-                C | Deck
-          </HeaderName>
-              <HeaderNavigation aria-label="C - Deck">
-                <HeaderMenuItem href="#">API</HeaderMenuItem>
-                <HeaderMenuItem href="#">Docs</HeaderMenuItem>
-                <HeaderMenuItem href="#">About</HeaderMenuItem>
-              </HeaderNavigation>
-              <HeaderGlobalBar>
-                <HeaderGlobalAction
-                  aria-label="Search">
-                  <Search20 />
-                </HeaderGlobalAction>
-                <HeaderGlobalAction
-                  aria-label="Notifications">
-                  <Notification20 />
-                </HeaderGlobalAction>
-                <HeaderGlobalAction
-                  aria-label="App Switcher">
-                  <AppSwitcher20 />
-                </HeaderGlobalAction>
-              </HeaderGlobalBar>
-              <SideNav aria-label="Side navigation" expanded={isSideNavExpanded}>
-                <SideNavItems>
-                  <SideNavLink large renderIcon={Grid32} element={Link} to="/">
-                    Dashboard
-              </SideNavLink>
-                  <SideNavLink large renderIcon={EmailNew32} element={Link} to="/sms">
-                    SMS
-              </SideNavLink>
-                  <SideNavLink large renderIcon={IotPlatform16} element={Link} to="/modem">
-                    Modems
-              </SideNavLink>
-                </SideNavItems>
-              </SideNav>
-            </Header>
-
-            <Content id="main-content" className="bx--col-lg-13 bx--offset-lg-3">
-              <Switch>
-                <Route exact path="/" component={DashBoard} />
-                <Route exact path="/sms" component={SMS} />
-                <Route exact path="/sms/:modem" />
-                <Route exact path="/new-sms" component={NewSMS} />
-                <Route exact path="/bulk-sms" component={BulkSMS} />
-                <Route exact path="/modem" component={Modem} />
-              </Switch>
-            </Content>
-          </>
-        )}
-      />
+      {view}
     </>
   );
 }
---
+++ src/content/Login/Login.js
@@ -0,0 +1,62 @@
+import React, { useRef } from 'react';
+import { Form, TextInput, Button } from 'carbon-components-react';
+import { ArrowRight24 } from '@carbon/icons-react';
+
+const Login = () => {
+
+    const username = useRef(null);
+    const password = useRef(null);
+
+    const formProps = {
+        onSubmit: (e) => {
+            e.preventDefault();
+            const loginData = {
+                username: username.current.value,
+                password: password.current.value
+            }
+
+            console.log(loginData);
+            //Todo : actually do something with this info
+        },
+    };
+
+    return (
+        <div className="bx--grid login-page__container">
+            <div className="bx--row">
+                <div className="bx--col-lg-7">
+                    <h1 className="login-page__title"><strong>C </strong> | Deck</h1>
+                    <p>login portal</p>
+
+                    <Form {...formProps}>
+                        <TextInput
+                            invalidText="Invalid error message."
+                            labelText="User Name"
+                            placeholder="enter username "
+                            id="username"
+                            ref={username}
+                        />
+                        <TextInput
+                            invalidText="Invalid error message."
+                            labelText="Password"
+                            placeholder="your password here"
+                            id="password"
+                            ref={password}
+                        />
+                        <Button
+                            className="submit-button"
+                            kind="primary"
+                            tabIndex={0}
+                            type="submit"
+                            renderIcon={ArrowRight24}
+                        >
+                            Continue
+                        </Button>
+                    </Form>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
