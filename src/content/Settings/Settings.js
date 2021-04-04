8521a6be7338e4802979cb2727ccd67c9fae1f15
nSedrickm <nsedrick101@gmail.com>
2021-04-04 15:50:24 +0100

    refactor: add error handlers for checking deku  state

--- src/content/Settings/Settings.js
+++ src/content/Settings/Settings.js
@@ -53,6 +53,27 @@ const Settings = () => {
             .then(response => {
                 setServiceState(response.state);
             })
+            .catch((error) => {
+                // Error 😨
+                if (error.response) {
+                    /*
+                     * The request was made and the server responded with a
+                     * status code that falls out of the range of 2xx
+                     */
+                    setServiceState("inactive");
+
+                } else if (error.request) {
+                    /*
+                     * The request was made but no response was received, `error.request`
+                     * is an instance of XMLHttpRequest in the browser and an instance
+                     * of http.ClientRequest in Node.js
+                     */
+                    setServiceState("failed");
+                } else {
+                    // Something happened in setting up the request and triggered an Error
+                    setServiceState("failed");
+                }
+            });
     }, []);
 
 
