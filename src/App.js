2857053e4eaebb9ae540794b43e0fe7d4de76378
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-11-22 22:25:26 +0100

    refactor: move general app styles to styles directory

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
-import "./App.scss";
-
+import React, { Fragment } from "react";
+import "./styles/App.scss";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import { DashBoard } from "./content";
 
 const App = () => {
-  return <DashBoard />
+  return (
+    <Fragment>
+      <Toaster position="bottom-right" reverseOrder={false} />
+      <BrowserRouter>
+        <DashBoard />
+      </BrowserRouter>
+    </Fragment>
+  )
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.scss";
+import App from "./App";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  <App />,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
