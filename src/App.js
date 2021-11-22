98a97bfbb5eb5edd79114f3ea6ffb7d38b5b94e3
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-11-22 21:51:32 +0100

    refactor: remove auth component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import React, { useState } from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
 
-import Login from './content/Auth';
-import DashBoard from './content/Dashboard';
-import { getToken } from './services/auth.service';
+import { DashBoard } from "./content";
 
 const App = () => {
-  const token = getToken();
-  const [isLoggedIn, setIsLoggedIn] = useState(token ? true : false);
-
-  if (isLoggedIn) {
-    return (<DashBoard setIsLoggedIn={setIsLoggedIn} />);
-  }
-  return (<Login setIsLoggedIn={setIsLoggedIn} />);
-};
-
+  return <DashBoard />
+}
 export default App;
